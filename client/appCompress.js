97cf48266df8d2c3b472c3d2695a3102faa4fec2
Ricardo Robles <rroblf01@estudiantes.unileon.es>
2024-03-10 19:49:22 +0100

    Add resize option

--- client/appCompress.js
+++ client/appCompress.js
@@ -2,8 +2,11 @@ class AppCompress extends HTMLElement {
   constructor() {
     super();
 
+    this.originalWidth = undefined;
+    this.originalHeight = undefined;
+
     this.innerHTML = /*html*/`<div class="flex flex-col items-center justify-center p-12">
-      <div class="mx-auto w-full max-w-[550px]">
+      <div class="mx-auto w-full">
         <h1 class="text-3xl font-bold text-[#6A64F1] text-center mb-8">Compress Image</h1>
         <form action="/createElement" method="POST" id="formApi">
           <div class="flex flex-col items-center justify-center">
@@ -13,6 +16,7 @@ class AppCompress extends HTMLElement {
               name="file"
             />
             <img id="image" class="hidden" />
+            <img id="hidenImage" class="hidden" />
             <p class="text-[#6A64F1] text-center mt-3" id="sizeOldImage"></p>
             <div>
             <span id="qualityValue">Calidad: 50</span>
@@ -23,6 +27,10 @@ class AppCompress extends HTMLElement {
               <option value="jpeg">JPEG</option>
               <option value="png">PNG</option>
             </select>
+            </div>
+            <div>
+            <span id="sizeValue">Size: 100</span>
+            <input type="range" step="1" value="100" min="1" max="100" id="size">
             </div>
             <button
               class="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white mt-3"
@@ -44,11 +52,20 @@ class AppCompress extends HTMLElement {
 
   connectedCallback() {
     const file = this.querySelector('#file');
-    const image = this.querySelector('#image');
 
     this.querySelector('#quality').addEventListener('input', (e) => {
       const qualityValue = this.querySelector('#qualityValue');
       qualityValue.textContent = `Calidad: ${e.target.value}`;
+    });
+
+    this.querySelector('#size').addEventListener('input', (e) => {
+      const sizeValue = this.querySelector('#sizeValue');
+      sizeValue.textContent = `Size: ${e.target.value}`;
+      const image = document.getElementById('image');
+      const hidenImage = this.querySelector('#hidenImage');
+
+      image.width = hidenImage.width * (e.target.value / 100);
+      image.height = hidenImage.height * (e.target.value / 100);
     });
 
     const humanFileSize = (bytes, dp = 1) => {
@@ -73,9 +90,13 @@ class AppCompress extends HTMLElement {
       const fileTarget = e.target.files[0];
       const reader = new FileReader();
       const sizeOldImage = this.querySelector('#sizeOldImage');
+      const image = this.querySelector('#image');
+      const hidenImage = this.querySelector('#hidenImage');
 
       reader.onload = function (e) {
         image.src = e.target.result;
+        hidenImage.src = e.target.result;
+
         image.classList.remove('hidden');
         sizeOldImage.textContent = `Size: ${humanFileSize(fileTarget.size)}`;
       };
@@ -88,11 +109,13 @@ class AppCompress extends HTMLElement {
       buttonPost.textContent = 'Loading...';
 
       const ext = this.querySelector('#formatExt');
-      const blob = await this.compressImage(file.files[0], quality.value, ext.value);
+      const fileForm = e.target.file.files[0];
+      const hidenImage = this.querySelector('#hidenImage');
+      const blob = await this.compressImage(fileForm, quality.value, ext.value);
 
       this.querySelector('#imageCompressed').src = URL.createObjectURL(blob);
       this.querySelector('#newImage').classList.remove('hidden');
-      const diffPercentage = (((file.files[0].size - blob.size) / file.files[0].size) * 100).toFixed(2);
+      const diffPercentage = (((fileForm.size - blob.size) / fileForm.size) * 100).toFixed(2);
       const message = diffPercentage > 0 ? `Reduced ${diffPercentage}%` : `Increased ${Math.abs(diffPercentage)}%`;
 
       this.querySelector('#sizeNewImage').textContent = `Size: ${humanFileSize(blob.size)} (${message})`;
@@ -105,10 +128,12 @@ class AppCompress extends HTMLElement {
     return new Promise((resolve, reject) => {
       const $canvas = document.createElement("canvas");
       const imagen = new Image();
-      imagen.onload = () => {
-        $canvas.width = imagen.width;
-        $canvas.height = imagen.height;
-        $canvas.getContext("2d").drawImage(imagen, 0, 0);
+      const imageResize = document.getElementById('image');
+      this.querySelector('#size')
+      imagen.onload = (e) => {
+        $canvas.width = imageResize.width;
+        $canvas.height = imageResize.height;
+        $canvas.getContext("2d").drawImage(imagen, 0, 0, imageResize.width, imageResize.height);
         $canvas.toBlob(
           (blob) => {
             if (blob === null) {
